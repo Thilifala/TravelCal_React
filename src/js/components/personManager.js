888e1b6ecaa4bfb8259efed1bc43a44c14c788de
Thilifala <Thilifala@outlook.com>
2017-08-04 18:02:22 +0800

    添加Person页组件

--- src/js/components/personManager.js
+++ src/js/components/personManager.js
@@ -4,17 +4,44 @@ import '../../css/personManager.less';
 import Title from './title.js';
 import Footer from './footer.js';
 
-let personname = ['aaa','bbb'];
-
-let PersonCopn = React.createClass({
+let PersonRow = React.createClass({
     render:function(){
         return (
+            <div className="personRowBox">
+                <div>{this.props.personName}</div>
+                <input type="button" className="btnDelPer"/>
+            </div>
+        )
+    }
+})
+
+let PersonList = React.createClass({
+    render:function(){
+        let personRows = this.props.person.map((name,i) =>
+            <PersonRow personName={name} key={i}/>
+        );
+        return (
             <div>
+                {personRows}
+            </div>
+        )
+    }
+})
+
+let PersonCopn = React.createClass({
+    getInitialState:function(){
+        return {
+            personArr:["PersonA","PersonB"]
+        }
+    },
+    render:function(){
+        return (
+            <div className="personMngbox">
                 <Title title="人"/>
-                <div></div>
+                <PersonList person={this.state.personArr}/>
                 <Footer>
-                    <input type="button" id="addPerson" value="添加人"/>
-                    <input type="button" id="addOK" value="添加完成"/>
+                    <input type="button" className="btnAddPerson"/>
+                    <input type="button" className="btnAddPersonOK"/>
                 </Footer>
             </div>
         );
