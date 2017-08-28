e57fe23d45e49e88556af2aef90ace1523c9ee7f
Thilifala <Thilifala@outlook.com>
2017-08-28 13:09:03 +0800

    弹窗修改

--- src/js/components/consumManager.jsx
+++ src/js/components/consumManager.jsx
@@ -4,21 +4,42 @@ import Title from './title.jsx';
 import Footer from './footer.jsx';
 import '../../css/popupWin.less';
 
+let ConsumeItem = function (name, cost, paid, padiFor) {
+    this.name = name || '';
+    this.cost = cost || 0;
+    this.paid = paid || '';
+    this.padiFor = padiFor || [];
+}
+
 let ItemWin = React.createClass({
+    propTypes:{
+        consumeItem:React.PropTypes.instanceOf(ConsumeItem).isRequired
+    },
+    getInitialState: function () {
+        return {
+            editingItem: {}
+        }
+    },
+    handleEditOk:function(){
+        this.props.onEditItemOK(this.state.editingItem);
+    },
+    handleNameChange:function(aa,bb){
+        let cc=aa;
+    },
     render: function () {
         return (
             <div className="popupWin style2">
                 <div>
-                    <label>项目</label><input ref="txtItemName" type="text" onChange={this.handleNameChange} />
+                    <label>项目</label><input ref="txtItemName" type="text" value={this.props.consumeItem.name} onChange={this.handleNameChange} />
                 </div>
                 <div>
-                    <label>花费</label><input ref="txtCost" type="text" onChange={this.handleNameChange} />
+                    <label>花费</label><input ref="txtCost" type="text" value={this.props.consumeItem.cost} onChange={this.handleNameChange} />
                 </div>
                 <div>
-                    <label>付钱人</label><input ref="txtPaid" type="text" onChange={this.handleNameChange} />
+                    <label>付钱人</label><input ref="txtPaid" type="text" value={this.props.consumeItem.paid} onChange={this.handleNameChange} />
                 </div>
                 <div>
-                    <label>蹭钱的</label><input ref="txtPaidFor" type="text" onChange={this.handleNameChange} />
+                    <label>蹭钱的</label><input ref="txtPaidFor" type="text" value={this.props.consumeItem.padiFor.join(',')} onChange={this.handleNameChange} />
                 </div>
                 <input type="button" value="OK" onClick={this.handleEditOk} />
             </div>
@@ -29,15 +50,26 @@ let ItemWin = React.createClass({
 let ConsumCopn = React.createClass({
     getInitialState: function () {
         return {
+            isEditing:false,
+            editingItem:{},
             personArr: []
         }
     },
     componentDidMount: function () {
         //TODO:fetch PersonArr
     },
-    //添加项目
-    handleAddItem: function () {
-
+    //点击添加项目
+    handleAddItemClick: function () {
+        this.setState({
+            isEditing:true,
+            editingItem:new ConsumeItem()
+        })
+    },
+    //添加OK
+    handleEditItemOK:function(){
+        this.setState({
+            isEditing:false
+        })
     },
     //计算
     handleDoCalc: function () {
@@ -51,9 +83,9 @@ let ConsumCopn = React.createClass({
         return (
             <div>
                 <Title title="消费" />
-                <ItemWin />
+                { this.state.isEditing ? <ItemWin onEditItemOK={this.handleEditItemOK} consumeItem={this.state.editingItem}/> : '' }
                 <Footer>
-                    <input type="button" className="btnAddItem" onClick={this.handleAddItem} />
+                    <input type="button" className="btnAddItem" onClick={this.handleAddItemClick} />
                     <input type="button" className="btnDoCalc" onClick={this.handleDoCalc} />
                     <input type="button" className="btnBack" onClick={this.handleBack} />
                 </Footer>
