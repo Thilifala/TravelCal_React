e5a72bb8b22151f188bb53aa6766441f98a38d27
Thilifala <Thilifala@outlook.com>
2017-08-30 13:14:37 +0800

    修改弹窗

--- src/js/components/consumManager.jsx
+++ src/js/components/consumManager.jsx
@@ -11,35 +11,55 @@ let ConsumeItem = function (name, cost, paid, padiFor) {
     this.padiFor = padiFor || [];
 }
 
+//项目弹窗组件
 let ItemWin = React.createClass({
-    propTypes:{
-        consumeItem:React.PropTypes.instanceOf(ConsumeItem).isRequired
+    propTypes: {
+        consumeItem: React.PropTypes.instanceOf(ConsumeItem).isRequired,
+        personArr:React.PropTypes.array.isRequired
     },
     getInitialState: function () {
         return {
-            editingItem: {}
+            consumeItem: this.props.consumeItem
         }
     },
-    handleEditOk:function(){
+    // OK
+    handleEditOk: function () {
         this.props.onEditItemOK(this.state.editingItem);
     },
-    handleNameChange:function(aa,bb){
-        let cc=aa;
+    //项目
+    handleNameChange: function (e) {
+        let item = this.state.consumeItem;
+        item.name = this.refs.txtItemName.value;
+        this.setState({
+            consumeItem: item
+        })
+    },
+    //花费
+    handleCostChange: function (e) {
+
+    },
+    // 付钱人
+    handlePaidClick: function (e) {
+        console.log(this.props.personArr);
+    },
+    // 蹭钱人
+    handlePaidForClick: function (e) {
+
     },
     render: function () {
         return (
             <div className="popupWin style2">
                 <div>
-                    <label>项目</label><input ref="txtItemName" type="text" value={this.props.consumeItem.name} onChange={this.handleNameChange} />
+                    <label>项目</label><input ref="txtItemName" type="text" value={this.state.consumeItem.name} onChange={this.handleNameChange} />
                 </div>
                 <div>
-                    <label>花费</label><input ref="txtCost" type="text" value={this.props.consumeItem.cost} onChange={this.handleNameChange} />
+                    <label>花费</label><input ref="txtCost" type="text" value={this.state.consumeItem.cost} onChange={this.handleCostChange} />
                 </div>
                 <div>
-                    <label>付钱人</label><input ref="txtPaid" type="text" value={this.props.consumeItem.paid} onChange={this.handleNameChange} />
+                    <label>付钱人</label><div className="txtDiv txtPaid" onClick={this.handlePaidClick}>{this.state.consumeItem.paid}</div>
                 </div>
                 <div>
-                    <label>蹭钱的</label><input ref="txtPaidFor" type="text" value={this.props.consumeItem.padiFor.join(',')} onChange={this.handleNameChange} />
+                    <label>蹭钱的</label><div className="txtDiv txtPaidFor" onClick={this.handlePaidForClick}>{this.state.consumeItem.padiFor.join(',')}</div>
                 </div>
                 <input type="button" value="OK" onClick={this.handleEditOk} />
             </div>
@@ -47,11 +67,25 @@ let ItemWin = React.createClass({
     }
 });
 
+// 人 选择器组件
+let PersonSelector = React.createClass({
+    propTypes:{
+        personArr:React.PropTypes.array.isRequired,
+        isMutiSelect:React.PropTypes.bool.isRequired
+    },
+    render:function(){
+        return (
+            <div className="personSelector"></div>
+        );
+    }
+})
+
+//顶层组件
 let ConsumCopn = React.createClass({
     getInitialState: function () {
         return {
-            isEditing:false,
-            editingItem:{},
+            isEditing: false,
+            editingItem: {},
             personArr: []
         }
     },
@@ -61,14 +95,14 @@ let ConsumCopn = React.createClass({
     //点击添加项目
     handleAddItemClick: function () {
         this.setState({
-            isEditing:true,
-            editingItem:new ConsumeItem()
+            isEditing: true,
+            editingItem: new ConsumeItem()
         })
     },
     //添加OK
-    handleEditItemOK:function(){
+    handleEditItemOK: function () {
         this.setState({
-            isEditing:false
+            isEditing: false
         })
     },
     //计算
@@ -83,7 +117,7 @@ let ConsumCopn = React.createClass({
         return (
             <div>
                 <Title title="消费" />
-                { this.state.isEditing ? <ItemWin onEditItemOK={this.handleEditItemOK} consumeItem={this.state.editingItem}/> : '' }
+                {this.state.isEditing ? <ItemWin onEditItemOK={this.handleEditItemOK} consumeItem={this.state.editingItem} personArr={this.state.personArr}/> : ''}
                 <Footer>
                     <input type="button" className="btnAddItem" onClick={this.handleAddItemClick} />
                     <input type="button" className="btnDoCalc" onClick={this.handleDoCalc} />
