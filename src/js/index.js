f3886a723fa51524a5eb5c1d771d6e502be1e0dd
Thilifala <Thilifala@outlook.com>
2018-11-09 23:16:15 +0800

    test reducer+store

--- src/js/index.js
+++ src/js/index.js
@@ -4,18 +4,19 @@ import { AnimatedSwitch } from 'react-router-transition';
 import { HashRouter as Router,BrowserRouter,Switch,Route,IndexRoute,Redirect} from 'react-router-dom';
 
 import {createStore} from 'redux';
-import {changePopupWinState} from './reducer/reducer';
+import {popupWinControl} from './reducer/reducer';
 
 import App from './components/app.jsx';
 import PersonCopn from './components/personManager.jsx';
 import ConsumCopn from './components/consumManager.jsx';
 
 //redux test
-let store = createStore(changePopupWinState,{showPopupWin:false});
+let store = createStore(popupWinControl,{showPopupWin:false});
 store.subscribe(() => {
     console.log(store.getState())
 })
 window.store = store;
+//控制台可以通过以下代码修改store：window.store.dispatch({type:"SHOW_MODAL_PER",showPopupWin:true})
 
 class AppRouter extends React.Component {
     render() {
--- src/js/reducer/reducer.js
+++ src/js/reducer/reducer.js
@@ -1,10 +1,12 @@
-import {SHOW_MODAL_PER,showPerModal} from '../action/actions'
+import { SHOW_MODAL_PER, showPerModal } from '../action/actions'
 
-export function changePopupWinState(state={},action){
-    switch(action){
+export function popupWinControl(state = {}, action) {
+    switch (action.type) {
         case SHOW_MODAL_PER:
-        return Object.assign({},state,{
-            showPopupWin:true
-        })
+            return Object.assign({}, state, {
+                showPopupWin: action.showPopupWin
+            })
+        default:
+            return state;
     }
 }
